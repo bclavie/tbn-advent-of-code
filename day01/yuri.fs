31fb7a75dfb5a0a1244a624429a4c7ec4b416a41
Yuri Hoogeweg <yurihoogeweg@gmail.com>
2023-12-01 11:13:02 +0000

    better day 1 yuri

--- day01/yuri.fs
+++ day01/yuri.fs
@@ -1,7 +1,6 @@
 ﻿let input = System.IO.File.ReadAllLines("day01/input.txt")
 
 let digits = "123456789".ToCharArray()
-let spelledOutDigits = ["one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
 
 let part1 = 
     input
@@ -10,48 +9,34 @@ let part1 =
 
 printfn "Part 1: %A" part1
 
-let spelledOutDigitValue str =
+let part2Digits = ["1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine"]
+
+let digitToString str =
     match str with 
-    | "one" -> 1
-    | "two" -> 2
-    | "three" -> 3
-    | "four" -> 4
-    | "five" -> 5
-    | "six" -> 6
-    | "seven" -> 7
-    | "eight" -> 8
-    | "nine" -> 9
+    | "1" | "one" -> "1"
+    | "2" | "two" -> "2"
+    | "3" | "three" -> "3"
+    | "4" | "four" -> "4"
+    | "5" | "five" -> "5"
+    | "6" | "six" -> "6"
+    | "7" | "seven" -> "7"
+    | "8" | "eight" -> "8"
+    | "9" | "nine" -> "9"
     | _ -> failwith "Invalid spelled out digit"
 
-type DigitIndices = { Value: int; FirstIndex: int; LastIndex: int }
+type DigitIndices = { Value: string; FirstIndex: int; LastIndex: int }
 
-let lineToNumber (line: string) = 
-    let spelledOut = 
-        spelledOutDigits 
-        |> List.map (fun str -> { Value = spelledOutDigitValue str; FirstIndex = line.IndexOf(str); LastIndex = line.LastIndexOf(str) })
+let firstNumber indices =
+    (indices |> List.sortBy (fun x -> x.FirstIndex) |> List.head).Value
 
-    let numbers =
-        digits
-        |> List.ofArray
-        |> List.map (fun c -> { Value = c.ToString() |> int; FirstIndex = line.IndexOf(c); LastIndex = line.LastIndexOf(c) })
+let lastNumber indices =
+    (indices |> List.sortByDescending (fun x -> x.LastIndex) |> List.head).Value
 
-    let combined = 
-        List.append numbers spelledOut
+let lineToNumber (line: string) : int = 
+        part2Digits 
+        |> List.map (fun str -> { Value = digitToString str; FirstIndex = line.IndexOf(str); LastIndex = line.LastIndexOf(str) })
         |> List.filter (fun x -> x.FirstIndex > -1 && x.LastIndex > -1)
-
-    let firstNumber =
-        combined
-        |> List.sortBy (fun x -> x.FirstIndex)
-        |> List.head
-        |> (fun x -> x.Value)
-
-    let lastNumber = 
-        combined
-        |> List.sortByDescending (fun x -> x.LastIndex)
-        |> List.head
-        |> (fun x -> x.Value)
-
-    int (firstNumber.ToString() + lastNumber.ToString())
+        |> (fun x -> [firstNumber x ; lastNumber x ] |> String.concat "") |> int
 
 let part2 =
     input 

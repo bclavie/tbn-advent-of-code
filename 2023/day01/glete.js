3d4c15b133262f7670c9f337ab649363b30dcee1
HogmanGlenn <ghogman19@student.oulu.fi>
2023-12-02 16:08:39 +0200

    Day 1 complete (this time for real)

--- 2023/day01/glete.js
+++ 2023/day01/glete.js
@@ -6,9 +6,36 @@ const readFileLines = (filename) =>
 
 const filePath = path.resolve(__dirname, "info.txt");
 let arr = readFileLines(filePath);
-var result = 0;
+
+var txtToNum = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
 
 const arr2 = arr.map((line) => {
+  for (let i = 0; i < line.length; i++) {
+    line = line
+      .replace(/one/g, txtToNum.one)
+      .replace(/two/g, txtToNum.two)
+      .replace(/three/g, txtToNum.three)
+      .replace(/four/g, txtToNum.four)
+      .replace(/five/g, txtToNum.five)
+      .replace(/six/g, txtToNum.six)
+      .replace(/seven/g, txtToNum.seven)
+      .replace(/eight/g, txtToNum.eight)
+      .replace(/nine/g, txtToNum.nine)
+      .replace(/tw1/g, "21")
+      .replace(/eigh2/g, "82")
+      .replace(/1ight/g, "18");
+  }
+
   const firstAndLast = line.match(/\d/g);
 
   const firstChar = firstAndLast && firstAndLast[0] ? firstAndLast[0] : "";
@@ -17,7 +44,7 @@ const arr2 = arr.map((line) => {
       ? firstAndLast[firstAndLast.length - 1]
       : "";
 
-  for (let i = 0; i <= line.length - 1; i++) {
+  for (let i = 0; i < line.length; i++) {
     if (lastChar != "") {
       return parseInt(firstChar + lastChar);
     } else {
@@ -26,9 +53,8 @@ const arr2 = arr.map((line) => {
   }
 });
 
-for (let i = 0; i <= arr2.length - 1; i++) {
-  result += arr2[i];
-}
+let result = 0;
 
-//console.log(arr2);
-//console.log(result);
+for (let i = 0; i < arr2.length; i++) {
+  result += parseInt(arr2[i]);
+}

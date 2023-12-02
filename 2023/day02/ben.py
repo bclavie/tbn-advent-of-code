20e65ae0923a283810ffb98f5927cb9d60a56037
bclavie <ben.clavie@brightnetwork.co.uk>
2023-12-02 12:55:42 +0000

    ben day 2

---
+++ 2023/day02/ben.py
@@ -0,0 +1,27 @@
+import math
+import re
+from collections import defaultdict
+
+
+def solve(lines: list[str], part1_limits: dict[str, int]) -> int:
+    part1_total = 0
+    part2_powers = 0
+    for game_id, line in enumerate(lines):
+        num_pairs = len(re.findall(r",", line))
+        print(f"Game {game_id + 1}: {num_pairs} pairs")
+        bag = defaultdict(int)
+        for num, col in re.findall(r"(\d+) (\w+)", line):
+            bag[col] = max(bag[col], int(num))
+        if not any(bag[col] > part1_limits[col] for col in part1_limits):
+            part1_total += game_id + 1
+        part2_powers += math.prod(bag.values())
+    return part1_total, part2_powers
+
+
+if __name__ == "__main__":
+    part1, part2 = solve(
+        lines=open("inputs.txt", "r").readlines(),
+        part1_limits={"red": 12, "green": 13, "blue": 14},
+    )
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")

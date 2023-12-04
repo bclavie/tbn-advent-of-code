2057cd27e6a9fdc58fe94f4156deda6429329c2d
bclavie <ben@clavie.eu>
2023-12-04 12:28:35 +0000

    ben day 4

---
+++ 2023/day04/ben.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+cards = [x.strip() for x in open("input.txt", "r").readlines()]
+part_1_score = 0
+card_copies = defaultdict(int)
+
+for current_card_idx, card in enumerate(cards):
+    card_copies[current_card_idx] += 1
+    winners, hand = (
+        set(int(x) for x in card_group.split())
+        for card_group in card.split(": ")[1].split(" | ")
+    )
+
+    part_1_score += 2 ** (len(winners & hand) - 1) if winners & hand else 0
+
+    for future_card_idx in range(1, len(winners & hand) + 1):
+        card_copies[current_card_idx + future_card_idx] += card_copies[current_card_idx]
+
+print(f"Part 1: {part_1_score}")
+print(f"Part 2: {sum(card_copies.values())}")

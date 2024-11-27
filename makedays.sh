aeb7f17858537954a31be89ad7ab9913dfe736d0
Benjamin Clavié <ben@clavie.eu>
2024-11-27 09:47:39 +0900

    2024

--- makedays.sh
+++ makedays.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+if [ $# -eq 0 ]; then
+  echo "Please provide a year as an argument"
+  exit 1
+fi
+
+year=$1
+mkdir -p "$year"
 for i in {1..24}; do
   folder_name=$(printf "day%02d" "$i")
-  mkdir "$folder_name"
-  touch "$folder_name/.gitkeep"
+  mkdir -p "$year/$folder_name"
+  touch "$year/$folder_name/.gitkeep"
 done

7f0188b1af9137e2337751cc5b3153bbf1dbeb2d
YuriHoogeweg <yurihoogeweg@gmail.com>
2025-12-01 08:32:28 +0000

    expand makedays.sh to account for shorter AoCs and add dirs for 2025

--- makedays.sh
+++ makedays.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-  echo "Please provide a year as an argument"
+
+# Require: year and number of days
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <year> <days>"
   exit 1
 fi
 
 year=$1
+days=$2
+
+# Ensure 'days' is a number
+case $days in
+  ''|*[!0-9]*)
+    echo "Days must be a numeric value"
+    exit 1
+    ;;
+esac
+
 mkdir -p "$year"
-for i in {1..24}; do
+
+i=1
+while [ $i -le $days ]; do
   folder_name=$(printf "day%02d" "$i")
   mkdir -p "$year/$folder_name"
   touch "$year/$folder_name/.gitkeep"
+  i=$((i + 1))
 done
+

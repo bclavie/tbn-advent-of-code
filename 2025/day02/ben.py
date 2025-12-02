fe55cb4396e61b6ebd90f11db846949166be35e8
bclavie <ben@clavie.eu>
2025-12-02 17:21:37 +0900

    ben better day2

--- 2025/day02/ben.py
+++ 2025/day02/ben.py
@@ -1,32 +1,33 @@
-import bisect
-
 def parse_lines(lines):
     return [[int(k) for k in x.split("-")] for x in lines.split(",")]    
 
+def generate(a, b, w):
+    L = len(str(a))
+    shift = 10**(L - w)
+    vals = [int(str(r) * (L // w)) for r in range(a // shift, b // shift + 1)]
+    return [val for val in vals if a <= val <= b]
+
 def process(inputs):
-    limit = max(r[1] for r in inputs)
-    len_limit = len(str(limit))
-    p1, p2 = set(), set()
-    for L in range(1, len_limit // 2 + 1):
-        for base in range(10**(L-1), 10**L):
-            s = str(base)
-            val_s = s * 2
-            while len(val_s) <= len_limit:
-                val = int(val_s)
-                if val <= limit:
-                    if len(val_s) == 2 * L:
-                        p1.add(val) 
-                    p2.add(val)                        
-                val_s += s
-
-    return sorted(p1), sorted(p2)
-
-def tally(part, inputs):
-    return sum(sum(n for n in part[bisect.bisect_left(part, x):bisect.bisect_right(part, y)]) for x, y in inputs)
+    p1, p2 = 0, 0
+    for x, y in inputs:
+        curr = x
+        while curr <= y:
+            L = len(str(curr))
+            end = min(y, 10**L - 1)
+            if L % 2 == 0:
+                p1 += sum(generate(curr, end, L // 2))
+            unique_vals = set()
+            for base in range(1, (L // 2) + 1):
+                if L % base == 0:
+                    generated = generate(curr, end, base)
+                    unique_vals.update(generated)
+            p2 += sum(unique_vals)
+            curr = end + 1
+    return p1, p2
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         inputs = parse_lines(f.read())
     p1, p2 = process(inputs)
-    print(f"Part 1: {tally(p1, inputs)}")
-    print(f"Part 2: {tally(p2, inputs)}")
+    print(f"Part 1: {p1}")
+    print(f"Part 2: {p2}")

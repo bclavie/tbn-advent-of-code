3f758fa9490d183020e372a14813aaac8678ec6b
Ren Binden <ren.binden@gmail.com>
2025-12-03 12:35:27 +0000

    functional solution

--- 2025/day03/ren.kts
+++ 2025/day03/ren.kts
@@ -1,30 +1,20 @@
 import java.io.File
 
-data class Bank(val joltages: List<Int>) {
-    fun findBankJoltage(batteryCount: Int): Long {
-        var result = ""
-        var pos = 0
-        while (result.length < batteryCount) {
-            val windowSize = joltages.size - pos - (batteryCount - result.length) + 1
-            var maxDigit = 0
-            var maxPos = pos
-            for (i in pos until pos + windowSize) {
-                if (joltages[i] > maxDigit) {
-                    maxDigit = joltages[i]
-                    maxPos = i
-                }
-            }
 
-            result += maxDigit
-            pos = maxPos + 1
-        }
-        return result.toLong()
+fun findBankJoltage(bank: List<Int>, batteryCount: Int): Long {
+    tailrec fun select(pos: Int, remaining: Int, acc: String = ""): String {
+        if (remaining == 0) return acc
+        val windowSize = bank.size - pos - remaining + 1
+        val window = bank.drop(pos).take(windowSize)
+        val maxDigit = window.max()
+        val maxPos = pos + window.indexOf(maxDigit)
+        return select(maxPos + 1, remaining - 1, acc + maxDigit)
     }
+    return select(0, batteryCount).toLong()
 }
 
 val totalJoltage = File("./input.txt").readLines()
     .filterNot(CharSequence::isBlank)
-    .map { bank -> Bank(bank.map { char -> char.toString().toInt() }) }
-    .sumOf { bank -> bank.findBankJoltage(12) }
+    .sumOf { bank -> findBankJoltage(bank.map { char -> char.toString().toInt() }, 12) }
 
 println("total joltage: $totalJoltage")
